290ead6e996bf81c0389c523411112b492e20ee0
einjojo <einjojo@proton.me>
2024-09-19 17:28:26 +0200

    Added methods to see how long a realm has been active

--- src/main/java/de/kalypzo/realms/realm/ActiveRealmWorld.java
+++ src/main/java/de/kalypzo/realms/realm/ActiveRealmWorld.java
@@ -3,19 +3,34 @@ package de.kalypzo.realms.realm;
 
 import de.kalypzo.realms.player.RealmPlayer;
 import de.kalypzo.realms.world.WorldHandle;
+import de.kalypzo.realms.world.WorldObserver;
 
+import java.time.Duration;
 import java.util.List;
 
 /**
  * Represents an active realm-world, and it provides a handle to interact with the world.
  * A Realm world becomes active when it is loaded on the current host.
  */
-public interface ActiveRealmWorld extends RealmWorld {
+public interface ActiveRealmWorld extends RealmWorld, WorldObserver {
 
     /**
      * @return the handle to interact with the world.
      */
     WorldHandle getWorldHandle();
+
+    /**
+     * Returns the timestamp when the realm-world was loaded.
+     * The timestamp is in milliseconds since the epoch.
+     *
+     * @return the timestamp when the realm-world was loaded.
+     */
+    long getLoadedTimestamp();
+
+    /**
+     * @return the duration since the realm-world was loaded.
+     */
+    Duration getUptime();
 
     /**
      * Returns whether the realm-world is currently idle.
