3f06776ea7cbe74c972ecd41d7bdffe7e1b358b3
einjojo <einjojo@proton.me>
2024-09-19 17:32:23 +0200

    stuff

--- src/main/java/de/kalypzo/realms/RealmPlugin.java
+++ src/main/java/de/kalypzo/realms/RealmPlugin.java
@@ -1,6 +1,25 @@
 package de.kalypzo.realms;
 
+import de.kalypzo.realms.config.RealmPluginConfiguration;
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class RealmPlugin  extends JavaPlugin {
+/**
+ * Loads configuration and initializes the correct RealmAPI implementation
+ */
+@Getter
+public class RealmPlugin extends JavaPlugin {
+    private RealmPluginConfiguration realmPluginConfiguration;
+
+    @Override
+    public void onEnable() {
+        RealmAPIProvider.setInstance(new ForwardingRealmAPI(this));
+
+    }
+
+    public boolean isHostingRealms() {
+        return !(realmPluginConfiguration.getRealmPluginMode() == RealmPluginMode.FORWARDING);
+    }
+
+
 }
---
+++ src/main/java/de/kalypzo/realms/config/RealmPluginConfiguration.java
@@ -0,0 +1,12 @@
+package de.kalypzo.realms.config;
+
+import de.kalypzo.realms.RealmPluginMode;
+
+public interface RealmPluginConfiguration {
+
+    /**
+     * @return the mode of the plugin
+     */
+    RealmPluginMode getRealmPluginMode();
+
+}
