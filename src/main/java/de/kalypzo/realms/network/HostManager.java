1dfaf0de1cac648ca0a8928087a4bf0826fa2c07
einjojo <einjojo@proton.me>
2024-09-22 11:52:59 +0200

    host and host discovery stuff

---
+++ src/main/java/de/kalypzo/realms/network/HostManager.java
@@ -0,0 +1,11 @@
+package de.kalypzo.realms.network;
+
+public class HostManager {
+
+
+    public void runDiscovery() {
+
+    }
+
+
+}
--- src/main/java/de/kalypzo/realms/network/RealmHost.java
+++ src/main/java/de/kalypzo/realms/network/RealmHost.java
@@ -10,6 +10,8 @@ import java.util.List;
  */
 public interface RealmHost {
 
+    int getHostNumericOrder();
+
     /**
      * @return the host's id
      */
